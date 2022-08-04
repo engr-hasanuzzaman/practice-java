cdff6f9ae131ed86eff2833780ae4d4891ebf987
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2022-08-04 23:15:49 +0600

    Basic while loop

---
+++ darek/tutorial/LessonFour.java
@@ -0,0 +1,12 @@
+package darek.tutorial;
+
+public class LessonFour {
+    public static void main(String[] args) {
+        // print 1 to 10 using while loop
+        int start = 1;
+        while (start <= 10) {
+            System.out.print(start + " ");
+            start += 1;
+        }
+    }
+}
