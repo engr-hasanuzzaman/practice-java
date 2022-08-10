816c267d5c96f59831e5d8d3ff04bc9ac59d9e08
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2022-08-10 22:59:47 +0600

    Add example of Supplier

--- functional/im/_Supplier.java
+++ functional/im/_Supplier.java
@@ -1,11 +1,14 @@
 package functional.im;
 
+import java.util.List;
 import java.util.function.Supplier;
 
 public class _Supplier {
     public static void main(String[] args) {
         System.out.println("You can access the following link " + localhost.get());
+        System.out.println("All the links " + urls.get());
     }
 
     private static Supplier<String> localhost = () -> "http://localhost:3000";
+    private static Supplier<List<String>> urls = () -> List.of("http://localhost:3000");
 }
