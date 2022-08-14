ac22ca561a2b3f534c6f93cc8be0c25e8866ec83
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2022-08-14 13:50:18 +0600

    More practice

--- functional/fs/_Options.java
+++ functional/fs/_Options.java
@@ -7,10 +7,15 @@ public class _Options {
     public static void main(String[] args) {
         Person man = new Person("Hasan", "Zaman");
         Person noLname = new Person("Hasan", null);
-        man.getlName().ifPresentOrElse(lName -> System.out.println("last name is " + lName), () -> System.out.println("Default lname"));
-        noLname.getlName().ifPresentOrElse(lName -> System.out.println("last name is " + lName), () -> System.out.println("Default lname"));
+        man.getlName().ifPresentOrElse(lName -> System.out.println("last name is " + lName),
+                () -> System.out.println("Default lname"));
+        noLname.getlName().ifPresentOrElse(lName -> System.out.println("last name is " + lName),
+                () -> System.out.println("Default lname"));
+
+        Object obj = Optional.ofNullable("Main value").orElseGet(() -> "Default Value from supplier");
+        System.out.println("Print the obj: " + obj);
     }
-   
+
 }
 
 /**
