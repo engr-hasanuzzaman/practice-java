cb54df89930f39b306daa2f485a2aaabbfebddc1
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2022-08-28 23:41:17 +0600

    Add dependency and run gradle class

--- darek/tutorial/FirstClass.java
+++ darek/tutorial/FirstClass.java
@@ -1,7 +1,8 @@
 package darek.tutorial;
-
+import org.apache.commons.lang3.StringUtils;
 public class FirstClass {
     public static void main(String[] args) {
-        System.out.println("Hello Java!");
+        String processedString = StringUtils.upperCase(String.join(" ", args));
+        System.out.println(processedString);
     }
 }
